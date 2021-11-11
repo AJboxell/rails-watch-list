a85f22c3dd41622047528053272f91e93a07c255
Alex Boxell <alexjboxell@gmail.com>
2021-11-11 11:52:29 +0000

    seeded movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+movies["results"].each do |film|
+  movie = Movie.new(title: film["original_title"], overview: film["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{film["poster_path"]}", rating: film["vote_average"])
+  puts "Adding #{film["original_title"]}..."
+  movie.save!
+end
+puts "Done"
