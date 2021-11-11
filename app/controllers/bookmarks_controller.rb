716c12df6dff7cb3c5c6c826882fb090db3f31e8
Alex Boxell <alexjboxell@gmail.com>
2021-11-11 16:17:15 +0000

    styled

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(comment: params[:bookmark]["comment"])
+    @bookmark.movie_id = params[:bookmark]["movie_id"]
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.delete
+    redirect_to @bookmark.list
+  end
+end
